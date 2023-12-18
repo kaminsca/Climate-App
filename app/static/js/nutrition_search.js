4b9788ee47f34ac03f527146c3bb916acef15d96
kaminsca <clark.a.kaminsky@vanderbilt.edu>
2023-12-17 21:37:05 -0600

    nutritionle random function

--- app/static/js/nutrition_search.js
+++ app/static/js/nutrition_search.js
@@ -11,6 +11,36 @@ $(document).ready(function() {
             showOptions(response);
         });
     });
+
+    $('#idForm').submit(function(e) {
+        e.preventDefault(); // Prevent the form from submitting
+        // Get the user's input
+        var id = $('#foodID').val();
+        clearPrevious();
+        setNutritionResults(id);
+    });
+
+    $('#randomFoodForm').submit(function(e) {
+        e.preventDefault(); // Prevent the form from submitting
+        GetRandomFood();
+    });
+
+    async function GetRandomFood(){
+        try {
+            let max = 999999;
+            let maxAttempts = 10;
+            const id = await findValidID(0, maxAttempts, max);
+            if (id === -1) {
+                console.log("Could not find valid id");
+                return;
+            }
+            clearPrevious();
+            setNutritionResults(id);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
     function showOptions(options){
         console.log("showing options: ");
         var buttonsHtml = '';
@@ -34,42 +64,8 @@ $(document).ready(function() {
 
         // Add click event handler for disambiguation options
         $('.option').click(function () {
-            if (!$("#summaryContainer").is(":visible")) {
-                $("#summaryContainer").toggle();
-            }
             var selectedQuery = $(this).data('query');
-            console.log(selectedQuery);
-            // Make a new AJAX request with the selected option
-            $.get('/get_nutrition_by_id', { query: selectedQuery}, function (response) {
-                // Display the summary for the selected option
-                console.log(response);
-                let serving;
-                if (response.servings.serving.length > 1){
-                    serving = response.servings.serving[0];
-                }
-                else{
-                    serving = response.servings.serving;
-                }
-                let text = "";
-                text += serving.serving_description;
-                if (serving.hasOwnProperty("metric_serving_amount")){
-                    text += " (" + Math.round(serving.metric_serving_amount) + " " + serving.metric_serving_unit + ")";
-                }
-                let details = ["calories", "protein", "sodium", "sugar", "carbohydrate", "fat", "fiber"];
-                details.forEach(function (nutrient, index) {
-                    if (serving.hasOwnProperty(nutrient)){
-                        const capitalized = nutrient.charAt(0).toUpperCase() + nutrient.slice(1);
-                        text += `<br />${capitalized}: ` + serving[nutrient];
-                        if (nutrient === "sodium" || nutrient === "cholesterol"){
-                            text += "mg";
-                        }
-                        else{
-                            text += "g";
-                        }
-                    }
-                });
-                $('#summary').html(text);
-            });
+            setNutritionResults(selectedQuery);
         });
     }
 
@@ -80,4 +76,79 @@ $(document).ready(function() {
             $("#summaryContainer").toggle();
         }
     }
+
+    function setNutritionResults(selectedQuery){
+        console.log("setting ")
+        console.log(selectedQuery);
+        // Make a new AJAX request with the selected option
+        $.get('/get_nutrition_by_id', { query: selectedQuery}, function (response) {
+            // Display the summary for the selected option
+            console.log(response);
+            if(response){
+                if (!$("#summaryContainer").is(":visible")) {
+                    $("#summaryContainer").toggle();
+                }
+            }
+            setFoodName(response);
+            let serving;
+            if (response.servings.serving.length > 1){
+                serving = response.servings.serving[0];
+            }
+            else{
+                serving = response.servings.serving;
+            }
+            let text = "";
+            text += serving.serving_description;
+            if (serving.hasOwnProperty("metric_serving_amount")){
+                text += " (" + Math.round(serving.metric_serving_amount) + " " + serving.metric_serving_unit + ")";
+            }
+            let details = ["calories", "protein", "sodium", "sugar", "carbohydrate", "fat", "fiber"];
+            details.forEach(function (nutrient, index) {
+                if (serving.hasOwnProperty(nutrient)){
+                    const capitalized = nutrient.charAt(0).toUpperCase() + nutrient.slice(1);
+                    text += `<br />${capitalized}: ` + serving[nutrient];
+                    if (nutrient === "sodium" || nutrient === "cholesterol"){
+                        text += "mg";
+                    }
+                    else{
+                        text += "g";
+                    }
+                }
+            });
+            $('#summary').html(text);
+        });
+    }
+
+    function setFoodName(food){
+        let myFood = food.food_type + " " + food.food_name;
+        if (food.hasOwnProperty("brand_name")) {
+            myFood = food.brand_name + " " + food.food_name;
+        }
+        document.getElementById("foodName").innerHTML = myFood;
+    }
+
+    function findValidID(attempts, maxAttempts, max) {
+        return new Promise((resolve, reject) => {
+            if (attempts >= maxAttempts) {
+                console.log("Could not find valid id");
+                //return -1;
+                resolve(-1);
+                return;
+            }
+            let id = Math.floor(Math.random() * max);
+            $.get('/get_nutrition_by_id', {query: id}, function (response, status) {
+                if (status === "success") {
+                    console.log("Success! id: ", id, " --- Data:", response);
+                    resolve(id);
+                    // return id;
+                } else {
+                    // return findValidID(attempts + 1, maxAttempts, max); // Make the next AJAX call
+                    findValidID(attempts + 1, maxAttempts, max)
+                    .then(resolve)
+                    .catch(reject);
+                }
+            });
+        });
+    }
+
 });
